5045b0fee93e526d38cf3f992760b83f4d07bf2c
jesusdavidgarciameza17-cloud <jesusdavidgarciameza17@gmail.com>
2026-02-13 18:04:13 -0500

    Update game.js passed to english

--- game.js
+++ game.js
@@ -1,40 +1,38 @@
-
 console.log("gambling")
 console.log("no entendi")
 
 
 const min = 1;
 const max = 456;
-const correcto = Math.floor(Math.random() * (max - min + 1) + min);  //
+const correct = Math.floor(Math.random() * (max - min + 1) + min);  //
 
+const soClose = 3;      
+const close = 10;        
+const far = 50;        
 
-const muyCerca = 3;      
-const cerca = 10;        
-const lejos = 35;        
+function tryGuess() {
+    const value = parseInt(document.getElementById("intento").value);
+    const difference = Math.abs(value - correct);
+    const hint = document.getElementById("pista");
 
-function probar() {
-    const valor = parseInt(document.getElementById("intento").value);
-    const diferencia = Math.abs(valor - correcto);
-    const pista = document.getElementById("pista");
-
-    if (isNaN(valor)) {
-        pista.textContent = "coloca un numero we un numero";
+    if (isNaN(value)) {
+        hint.textContent = "coloca un numero we un numero";
         return;
     }
 
-    if (valor === correcto) {
-        pista.textContent = "Ganaste, pero eso no cambiara nada";
+    if (value === correct) {
+        hint.textContent = "Ganaste, pero eso no cambiara nada";
         return;
     }
 
-    // pistas
-    if (diferencia <= muyCerca) {
-        pista.textContent = "en llamas como un volcan (muy cerca)";
-    } else if (diferencia <= cerca) {
-        pista.textContent = "stufa (cerca)";
-    } else if (diferencia <= lejos) {
-        pista.textContent = "frezzer (frio)";
+    // hints
+    if (difference <= soClose) {
+        hint.textContent = "en llamas como un volcan (muy cerca)";
+    } else if (difference <= close) {
+        hint.textContent = "stufa (cerca)";
+    } else if (difference <= far) {
+        hint.textContent = "frezzer (frio)";
     } else {
-        pista.textContent = "congelado (muy frio)";
+        hint.textContent = "congelado (muy frio)";
     }
 }
